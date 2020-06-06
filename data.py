ee0abe09d6292a02fd3f50c41a183dc756349860
regnartsss <4Kikbtyma>
2020-06-06 21:56:46 +0700

    fix gitignore

--- data.py
+++
@@ -1,5 +0,0 @@
-admin_id = [765333440]
-time_energy = 1500
-time_healts = 120
-time_step = 60
-start_bot = 0
--- handlers/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import general_commands
-from . import general_text
-from . import general_callback
-# from . import default_handler
